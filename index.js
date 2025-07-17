ef240b4622d31130bbb161f0604965538bcd510b
Shaurya Kumar <shoryakumar77812@gmail.com>
2025-07-18 02:41:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/what', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+  res.send('<h2>chai aur code! </h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
